7a4aa9a917275ff5742439fdaa5c7a292dbc736d
FaizanDurrani <faizandurrani324@gmail.com>
2021-07-23 18:00:32 +0000

    Deploying to gh-pages from @ Paperback-iOS/extensions-sources@a3c17661dc8da28333ef10f0ca7250da0c9c238d 🚀

--- primary/MangaDex/source.js
+++ primary/MangaDex/source.js
@@ -674,7 +674,7 @@ exports.MangaDexInfo = {
     description: 'Extension that pulls manga from MangaDex',
     icon: 'icon.png',
     name: 'MangaDex',
-    version: '2.0.9',
+    version: '2.0.10',
     authorWebsite: 'https://github.com/nar1n',
     websiteBaseURL: MANGADEX_DOMAIN,
     contentRating: paperback_extensions_common_1.ContentRating.EVERYONE,
@@ -880,7 +880,7 @@ class MangaDex extends paperback_extensions_common_1.Source {
                         .addQueryParameter('offset', offset)
                         .addQueryParameter('includes', ['scanlation_group'])
                         .addQueryParameter('translatedLanguage', languages)
-                        .addQueryParameter('order', { 'volume': 'desc', 'chapter': 'desc' })
+                        .addQueryParameter('order', { 'volume': 'desc', 'chapter': 'desc', 'publishAt': 'desc' })
                         .buildUrl(),
                     method: 'GET',
                 });
