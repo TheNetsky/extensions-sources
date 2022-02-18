aeaec913aaa4e1c2c85a6509eb4265568757ddd8
Faizan Durrani <faizandurrani324@gmail.com>
2022-02-18 22:18:38 +0500

    Update MangaDex.ts

--- src/MangaDex/MangaDex.ts
+++ src/MangaDex/MangaDex.ts
@@ -479,7 +479,7 @@ export class MangaDex extends Source {
         const sections = [
             {
                 request: createRequestObject({
-                    url: await this.getCustomListRequestURL('d434f5f1-ff90-4fa5-be7c-2c070da79120', ratings),
+                    url: await this.getCustomListRequestURL('ff210dec-862b-4c17-8608-0e7f97c70488', ratings),
                     method: 'GET',
                 }),
                 section: createHomeSection({
