2e589fa762acef1dfa2e748f8b0a1b71a655bb9f
TheNetsky <56271887+TheNetsky@users.noreply.github.com>
2022-06-23 17:29:27 +0200

    Update Webtoons.ts

--- src/Webtoons/Webtoons.ts
+++ src/Webtoons/Webtoons.ts
@@ -24,7 +24,7 @@ import { Parser } from './WebtoonsParser'
 
 const WEBTOONS_DOMAIN = 'https://www.webtoons.com'
 const WEBTOONS_MOBILE_DOMAIN = 'https://m.webtoons.com'
-const userAgent = 'Mozilla/5.0 (iPhone; CPU iPhone OS 15_4_1 like Mac OS X) AppleWebKit/605.1.15 (KHTML, like Gecko) Version/15.4 Mobile/15E148 Safari/604.1'
+const userAgent = 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/102.0.5005.124 Safari/537.36 Edg/102.0.1245.44'
 let langString = 'en' // Only used for 'getMangaShareUrl' function
 
 export const WebtoonsInfo: SourceInfo = {
